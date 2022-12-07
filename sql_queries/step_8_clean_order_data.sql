756d8c009438ab9cdeb8907850a4e3b3cddcaf35
Omar ElMaria <omar.elmaria@deliveryhero.com>
2022-12-07 15:15:55 +0100

    delete step 8 and amend step 9 + rename its sql query

--- sql_queries/step_8_clean_order_data.sql
+++
@@ -1,7 +0,0 @@
--- Step 8: Clean the orders data by filtering for records where keep_drop_flag = "Keep" (refer to the code above to see how this field was constructed)
-CREATE OR REPLACE TABLE `dh-logistics-product-ops.pricing.ab_test_individual_orders_cleaned_staging_lb_rollout_tests` AS
-SELECT
-    *
-FROM `dh-logistics-product-ops.pricing.ab_test_individual_orders_augmented_lb_rollout_tests`
-WHERE TRUE
-    AND keep_drop_flag_bi = "Keep"; -- Filter for the orders that have the correct target_group, variant, and scheme ID based on the configuration of the experiment
